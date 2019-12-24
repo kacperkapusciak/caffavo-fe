ba72b58fd4f0a1c787d27dde4c51f80500fab2ca
kacperkapusciak <kapusciak.k@gmail.com>
2019-12-24 23:31:57 +0100

    Workspace setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import GlobalStyle from './styles/GlobalStyle';
+
+function App() {
+  return (
+    <>
+      Hi
+      <GlobalStyle/>
+    </>
+  );
+}
+
+export default App;
